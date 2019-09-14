2151c7178b401ec6b708f9ffabc485e32539d12e
jwnicholas99 <jwnicholas99@gmail.com>
2019-09-14 19:30:13 -0400

    switched branches

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app, db
 from flask import render_template, url_for, redirect, flash, request
-from flask_login import login_required, current_user, login_user, logout_user
+from flask_login import current_user, login_user, logout_user
 from app.forms import LoginForm, RegistrationForm, DiaryForm
 from app.models import User, Diary
 from functools import wraps
@@ -53,9 +53,14 @@ def registration():
             return redirect(url_for('indexPatient'))
         form = RegistrationForm()
         if form.validate_on_submit():
-            user = User(username=form.username.data)
+            user = User(username=form.username.data, email=form.email.data)
+            user.is_psychiatrist = form.is_psychiatrist.data
             user.set_password(form.password.data)
-
+            db.session.add(user)
+            db.session.commit()
+            flash('Your accoutn has been registered!')
+            return redirect(url_for('login'))
+    return render_template('registration.html', title='Register', form=form)       
 
 
 
